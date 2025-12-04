3a3be06c6ee68b88b9fb47a4ae7c0139e0e76183
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-12-04 16:20:51 +0100

    nexus: Add AutoLoginMiddleware

---
+++ tests/nexus/test_middleware.py
@@ -0,0 +1,95 @@
+import pytest
+from django.contrib.auth.models import User
+from pytest_django.asserts import assertRedirects
+
+from itoutils.django.urls import add_url_params
+from itoutils.nexus.utils import generate_jwt
+from tests.django.factories import UserFactory
+
+params_tuples = [
+    pytest.param({}, id="no_params"),
+    pytest.param({"filter": "76", "username": "123"}, id="with_params"),
+]
+
+
+@pytest.mark.parametrize("params", params_tuples)
+def test_middleware_for_authenticated_user(db, client, params, caplog):
+    expected_url = add_url_params("/path", params)
+
+    user = UserFactory()
+    client.force_login(user)
+
+    response = client.get(add_url_params("/path", params | {"auto_login": generate_jwt(user)}))
+    assertRedirects(response, expected_url, fetch_redirect_response=False)
+    assert caplog.messages == ["Nexus auto login: user is already logged in"]
+
+
+@pytest.mark.parametrize("params", params_tuples)
+def test_middleware_for_wrong_authenticated_user(db, client, params, caplog):
+    expected_url = add_url_params("/path", params)
+
+    user = UserFactory()
+    # Another user is logged in
+    client.force_login(User.objects.create(email="moi@mailinator.com", pk=2))
+
+    response = client.get(add_url_params("/path", params | {"auto_login": generate_jwt(user)}))
+    assertRedirects(
+        response,
+        add_url_params("/proconnect/authorize", {"email": user.email, "next_url": expected_url}),
+        fetch_redirect_response=False,
+    )
+    assert caplog.messages == [
+        "Nexus auto login: wrong user is logged in -> logging them out",
+        f"Nexus auto login: {user} was found and forwarded to ProConnect",
+    ]
+
+
+def test_middleware_multiple_tokens(db, client, caplog):
+    user = UserFactory()
+    token = generate_jwt(user)
+    response = client.get(f"/path?auto_login={token}&auto_login={token}")
+    assertRedirects(response, "/path", fetch_redirect_response=False)
+    assert caplog.messages == [
+        "Nexus auto login: Multiple tokens found -> ignored",
+    ]
+
+
+def test_middleware_invalid_token(db, client, caplog):
+    params = {"auto_login": "bad jwt"}
+    response = client.get(add_url_params("/path", params))
+    assertRedirects(response, "/path", fetch_redirect_response=False)
+    assert caplog.messages == [
+        "Could not decrypt jwt",
+        "Invalid auto login token",
+        "Nexus auto login: Missing email in token -> ignored",
+    ]
+
+
+@pytest.mark.parametrize("params", params_tuples)
+def test_middleware_with_no_existing_user(db, client, params, caplog):
+    expected_url = add_url_params("/path", params)
+
+    user = UserFactory.build()
+    jwt = generate_jwt(user)
+    response = client.get(add_url_params("/path", params | {"auto_login": jwt}))
+    assertRedirects(
+        response,
+        add_url_params("/register", {"email": user.email, "next_url": expected_url}),
+        fetch_redirect_response=False,
+    )
+    assert caplog.messages == [f"Nexus auto login: no user found for jwt={jwt}"]
+
+
+@pytest.mark.parametrize("params", params_tuples)
+def test_middleware_for_unlogged_user(db, client, params, caplog):
+    expected_url = add_url_params("/path", params)
+
+    user = UserFactory()
+
+    response = client.get(add_url_params("/path", params | {"auto_login": generate_jwt(user)}))
+    assertRedirects(
+        response,
+        add_url_params("/proconnect/authorize", {"email": user.email, "next_url": expected_url}),
+        fetch_redirect_response=False,
+    )
+    assert caplog.messages == [f"Nexus auto login: {user} was found and forwarded to ProConnect"]
