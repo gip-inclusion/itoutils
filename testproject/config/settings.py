3346a057dba9764c25b7178c6cb08baf8db43bb7
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-12-10 14:07:50 +0100

    nexus: Add AutoLoginMiddleware

--- testproject/config/settings.py
+++ testproject/config/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "testproject.testapp.nexus.middleware.AutoLoginMiddleware",
 ]
 
 ROOT_URLCONF = "testproject.config.urls"
