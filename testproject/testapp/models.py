4cac24387fe5f8ac09a3e1b8683bcac30901e9ce
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-12 11:28:25 +0100

    django: Add HasDataChangedMixin
    Allows to track changes in instance, which is usefull when saving it if
    we want to trigger side effects (such as syncing the data)

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from itoutils.django.models import HasDataChangedMixin
 
-class Item(models.Model):
+
+class Item(models.Model, HasDataChangedMixin):
     parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="children", null=True)
+    category = models.CharField()
 
     class Meta:
         app_label = "testapp"
