b8d3a94f219831fc7655fb8b022f438a8aa06bae
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-22 15:36:49 +0100

    nexus: Add Nexus API client

--- src/itoutils/pytest.py
+++ src/itoutils/pytest.py
@@ -4,6 +4,7 @@ import logging
 from unittest import mock
 
 import pytest
+from django.utils import timezone
 
 
 @pytest.fixture
@@ -25,3 +26,20 @@ def capture_stream_handler_log(request):
 def mock_nexus_token():
     with mock.patch("itoutils.django.nexus.views.generate_auto_login_token", return_value="JWT") as mocked:
         yield mocked
+
+
+@pytest.fixture
+def mock_nexus_api(respx_mock, settings):
+    settings.NEXUS_API_BASE_URL = "http://nexus/api/"
+    settings.NEXUS_API_TOKEN = "very-secret-token"
+    respx_mock.post(f"{settings.NEXUS_API_BASE_URL}sync-start").respond(
+        200, json={"started_at": timezone.now().isoformat()}
+    )
+    respx_mock.post(f"{settings.NEXUS_API_BASE_URL}users").respond(200)
+    respx_mock.delete(f"{settings.NEXUS_API_BASE_URL}users").respond(200)
+    respx_mock.post(f"{settings.NEXUS_API_BASE_URL}structures").respond(200)
+    respx_mock.delete(f"{settings.NEXUS_API_BASE_URL}structures").respond(200)
+    respx_mock.post(f"{settings.NEXUS_API_BASE_URL}memberships").respond(200)
+    respx_mock.delete(f"{settings.NEXUS_API_BASE_URL}memberships").respond(200)
+    respx_mock.post(f"{settings.NEXUS_API_BASE_URL}sync-completed").respond(200)
+    return respx_mock
