13a421df476fbafb330c2e652da574c93f6a0505
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-28 20:57:20 +0100

    SQUASH_ME: improve tests

--- tests/django/nexus/test_sync.py
+++ tests/django/nexus/test_sync.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock
 
 import pytest
 
@@ -20,39 +21,42 @@ class TestSync:
         self.manager_mocked_sync.reset_mock()
         self.manager_mocked_delete.reset_mock()
 
+    def assert_mocked_calls(self, mock_name, args=None):
+        if mock_name:
+            assert getattr(self, mock_name).call_args_list == [unittest.mock.call(args)], (
+                f"Bad arguments for {mock_name}"
+            )
+        for non_called_mock_name in [
+            "model_mocked_sync",
+            "model_mocked_delete",
+            "manager_mocked_sync",
+            "manager_mocked_delete",
+        ]:
+            if non_called_mock_name != mock_name:
+                assert getattr(self, non_called_mock_name).call_args_list == [], (
+                    f"{mock_name} should not have been called"
+                )
+
     def test_sync_on_model_save(self, nexus_sync):
         synced_item = SyncedItemFactory()
 
-        assert self.model_mocked_sync.call_args_list == [unittest.mock.call([synced_item])]
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("model_mocked_sync", [synced_item])
 
         # saving while updating a non tracked field doesn't trigger an api call
         self.reset_mocks()
         synced_item.category = "other_category"
         synced_item.save()
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls(None)
 
         # saving while updating a tracked field triggers an api call
         self.reset_mocks()
         synced_item.user = UserFactory()
         synced_item.save()
-        assert self.model_mocked_sync.call_args_list == [unittest.mock.call([synced_item])]
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("model_mocked_sync", [synced_item])
 
     def test_delete_on_model_save(self, nexus_sync, mock_nexus_api):
         synced_item = SyncedItemFactory(sync_me=False)
-
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == [unittest.mock.call([synced_item.pk])]
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("model_mocked_delete", [synced_item.pk])
 
     def test_delete_on_model_delete(self, nexus_sync, mock_nexus_api):
         synced_item = SyncedItemFactory()
@@ -60,10 +64,7 @@ class TestSync:
         self.reset_mocks()
 
         synced_item.delete()
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == [unittest.mock.call([synced_item_id])]
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("model_mocked_delete", [synced_item_id])
 
     def test_sync_on_manager_update(self, nexus_sync, mock_nexus_api):
         synced_item_1 = SyncedItemFactory()
@@ -71,10 +72,7 @@ class TestSync:
         self.reset_mocks()
 
         SyncedItem.objects.order_by("pk").update(sync_me=True, user=UserFactory())
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == [unittest.mock.call([synced_item_1, synced_item_2])]
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("manager_mocked_sync", [synced_item_1, synced_item_2])
 
     def test_delete_on_manager_update(self, nexus_sync, mock_nexus_api):
         synced_item = SyncedItemFactory()
@@ -82,10 +80,7 @@ class TestSync:
 
         # Updating a field on the model
         SyncedItem.objects.update(sync_me=False)
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == [unittest.mock.call([synced_item.pk])]
+        self.assert_mocked_calls("manager_mocked_delete", [synced_item.pk])
 
         synced_item.user.is_active = False
         synced_item.user.save()
@@ -94,10 +89,7 @@ class TestSync:
         # Even if we pu back sync_me=True since the user is inactive, synced_item.should_sync_to_nexus will
         # return False and we will try to delete it again
         SyncedItem.objects.update(sync_me=True)
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == [unittest.mock.call([synced_item.pk])]
+        self.assert_mocked_calls("manager_mocked_delete", [synced_item.pk])
 
     def test_delete_on_manager_delete(self, nexus_sync, mock_nexus_api):
         synced_item_1 = SyncedItemFactory()
@@ -105,10 +97,7 @@ class TestSync:
         self.reset_mocks()
 
         SyncedItem.objects.order_by("pk").delete()
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == [unittest.mock.call([synced_item_1.pk, synced_item_2.pk])]
+        self.assert_mocked_calls("manager_mocked_delete", [synced_item_1.pk, synced_item_2.pk])
 
     def test_sync_on_manager_bulk_update(self, nexus_sync, mock_nexus_api):
         synced_item_1 = SyncedItemFactory()
@@ -116,10 +105,7 @@ class TestSync:
         self.reset_mocks()
 
         SyncedItem.objects.bulk_update([synced_item_1, synced_item_2], ["sync_me"])
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == [unittest.mock.call([synced_item_1, synced_item_2])]
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls("manager_mocked_sync", [synced_item_1, synced_item_2])
 
     def test_delete_on_manager_bulk_update(self, nexus_sync, mock_nexus_api):
         synced_item_1 = SyncedItemFactory()
@@ -130,10 +116,7 @@ class TestSync:
         synced_item_2.sync_me = False
 
         SyncedItem.objects.bulk_update([synced_item_1, synced_item_2], ["sync_me"])
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == [unittest.mock.call([synced_item_1.pk, synced_item_2.pk])]
+        self.assert_mocked_calls("manager_mocked_delete", [synced_item_1.pk, synced_item_2.pk])
 
     def test_bulk_create(self, nexus_sync, mock_nexus_api):
         user = UserFactory()
@@ -146,8 +129,4 @@ class TestSync:
         assert SyncedItem.objects.count() == 0
 
         SyncedItem.objects.bulk_create([synced_item_1, synced_item_2], nexus_no_sync=True)
-        assert SyncedItem.objects.count() == 2
-        assert self.model_mocked_sync.call_args_list == []
-        assert self.model_mocked_delete.call_args_list == []
-        assert self.manager_mocked_sync.call_args_list == []
-        assert self.manager_mocked_delete.call_args_list == []
+        self.assert_mocked_calls(None)
