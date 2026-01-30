899da90d4235f4a51e79a030ad9dd2ef7a7188bb
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-30 15:34:51 +0100

    nexus: Add model and manager sync mixin

--- src/itoutils/django/nexus/api.py
+++ src/itoutils/django/nexus/api.py
@@ -17,65 +17,33 @@ class NexusAPIClient:
             headers={"Authorization": f"Token {settings.NEXUS_API_TOKEN}"},
         )
 
-    def init_full_sync(self):
+    def call(self, method, url, **kwargs):
         try:
-            response = self.client.post("sync-start").raise_for_status()
+            return self.client.request(method, url, **kwargs).raise_for_status()
         except httpx.HTTPError as exc:
-            logger.exception("nexus init_full_sync error=%s", exc)
+            logger.exception(f"nexus {method}:{url} error=%s", exc)
             raise NexusAPIException from exc
-        return response.json()["started_at"]
+
+    def init_full_sync(self):
+        return self.call("POST", "sync-start").json()["started_at"]
 
     def complete_full_sync(self, start_at):
-        try:
-            self.client.post("sync-completed", json={"started_at": start_at}).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus complete_full_sync error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("POST", "sync-completed", json={"started_at": start_at})
 
     def send_users(self, users_data):
-        try:
-            self.client.post("users", json=users_data).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus send_users error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("POST", "users", json=users_data)
 
     def delete_users(self, user_pks):
-        try:
-            self.client.request(
-                "DELETE", "users", json=[{"id": str(user_pk)} for user_pk in user_pks]
-            ).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus delete_users error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("DELETE", "users", json=[{"id": str(user_pk)} for user_pk in user_pks])
 
     def send_structures(self, structures_data):
-        try:
-            self.client.post("structures", json=structures_data).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus send_structures error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("POST", "structures", json=structures_data)
 
     def delete_structures(self, structure_pks):
-        try:
-            self.client.request(
-                "DELETE", "structures", json=[{"id": str(structure_pk)} for structure_pk in structure_pks]
-            ).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus delete_structures error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("DELETE", "structures", json=[{"id": str(structure_pk)} for structure_pk in structure_pks])
 
     def send_memberships(self, memberships_data):
-        try:
-            self.client.post("memberships", json=memberships_data).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus send_memberships error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("POST", "memberships", json=memberships_data)
 
     def delete_memberships(self, membership_pks):
-        try:
-            self.client.request(
-                "DELETE", "memberships", json=[{"id": str(membership_pk)} for membership_pk in membership_pks]
-            ).raise_for_status()
-        except httpx.HTTPError as exc:
-            logger.exception("nexus delete_memberships error=%s", exc)
-            raise NexusAPIException from exc
+        self.call("DELETE", "memberships", json=[{"id": str(membership_pk)} for membership_pk in membership_pks])
