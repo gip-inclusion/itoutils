91fd99725541cab53bdba2b92ee06acefa2af237
Gérald Gounot <gerald@gounot.eu>
2026-04-23 18:39:50 +0200

    feat: add Découpage Administratif models, importer and command

---
+++ src/itoutils/django/decoupage_administratif/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DecoupageAdministratifConfig(AppConfig):
+    name = "itoutils.django.decoupage_administratif"
+    verbose_name = "Découpage administratif"
