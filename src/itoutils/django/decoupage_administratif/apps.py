83cfddc1ee588c52d92c5a5fbaee7bb3f19d259c
Gérald Gounot <gerald@gounot.eu>
2026-05-18 14:44:38 +0200

    chore: documentation de l'app decoupage_administratif

--- src/itoutils/django/decoupage_administratif/apps.py
+++ src/itoutils/django/decoupage_administratif/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class DecoupageAdministratifConfig(AppConfig):
+    # L'objectif de cette app est uniquement de pouvoir :
+    #   - analyser un code géographique via get_division_info_for_division_code()
+    #   - convertir une liste de codes géographiques en noms en français via get_division_label()
+    # Les données géographiques importées de l'API Découpage Administratif servent uniquement à cet usage
+    # et n'ont pas pour objectif de constituer une base de référence.
+    # Nous sommes conscients de la duplication de code avec sync_cities, à corriger à l'avenir.
     name = "itoutils.django.decoupage_administratif"
     verbose_name = "Découpage administratif"
